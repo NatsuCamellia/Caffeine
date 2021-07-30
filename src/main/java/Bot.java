316d09cb25c22b2533974c1738f1bcdc6bee55df
NatsuCamellia <natsucamellia@gmail.com>
2021-07-30 21:31:48 +0800

    Basic

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Bot {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+
+    public static void main(String[] args) throws Exception {
+
+        JDA jda = JDABuilder.createDefault("NzQxMTczMzI2NzA5NTIyNTAz.Xyztcw.FtmZ_XnGnQh2z7hM4rnIJis12X4").build();
+
+        jda.addEventListener(new Command());
+
+    }
 }
