7001ac1d3b55ecc09b64667e9e9c425cba54e142
NatsuCamellia <natsucamellia@gmail.com>
2021-07-31 22:24:54 +0800

    ping command

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,4 +1,7 @@
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -17,9 +20,43 @@ public class Command extends ListenerAdapter {
             event.getChannel().sendMessage(mention).queue();
 
         } else if (command.equalsIgnoreCase("getId")) {
+
             sendPrivateMessage(author, id);
+
+        } else if (command.equalsIgnoreCase("ping")) {
+
+            long time = System.currentTimeMillis();
+            
+            event.getChannel().sendMessage("Pinging...").queue(responce -> {responce.editMessageFormat("%d ms", System.currentTimeMillis() - time).queue();});
         }
 
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandEvent event) {
+        if (event.getName().equals("ping")) {
+
+            long time = System.currentTimeMillis();
+
+            event.reply("Pong!").setEphemeral(true) // reply or acknowledge
+             .flatMap(v ->
+                 event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
+             ).queue(); // Queue both reply and edit
+
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.isFromType(ChannelType.PRIVATE)) {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+        } else {
+            System.out.printf("[%s] [%s] %s: %s\n",
+                            event.getGuild().getName(),
+                            event.getTextChannel().getName(),
+                            event.getMember().getEffectiveName(),
+                            event.getMessage().getContentDisplay());
+        }
     }
 
     public void sendPrivateMessage(User user, String content) {
