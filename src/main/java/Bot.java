72dc9c6e0d9c27020a8251e78ce326b16cfb91a2
NatsuCamellia <natsucamellia@gmail.com>
2021-07-31 22:28:45 +0800

    add some configurations

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,13 +1,51 @@
+import javax.security.auth.login.LoginException;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Bot {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws LoginException, InterruptedException {
 
-        JDA jda = JDABuilder.createDefault("NzQxMTczMzI2NzA5NTIyNTAz.Xyztcw.FtmZ_XnGnQh2z7hM4rnIJis12X4").build();
+        if (args.length < 1) {
+            System.out.println("You have to provide TOKEN");
+        }
 
-        jda.addEventListener(new Command());
+        JDABuilder builder = JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES);
 
+        builder.setActivity(Activity.playing("調配咖啡"));
+
+        builder.addEventListeners(new Command());
+
+        configureMemoryUsage(builder);
+
+        JDA jda = builder.build();
+        
+        jda.upsertCommand("ping", "Caculate ping of the BOT").queue();
+    }
+
+    // 調整記憶體使用量
+    public static void configureMemoryUsage(JDABuilder builder) {
+        // 關閉成員動態快取 (直播/遊戲/spotify)
+        builder.disableCache(CacheFlag.ACTIVITY);
+
+        // 只儲存在語音頻道內的成員或伺服器擁有者的快取
+        builder.setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER));
+
+        // Disable member chunking on startup
+        builder.setChunkingFilter(ChunkingFilter.NONE);
+
+        // Disable presence updates and typing events
+        builder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
+
+        // Consider guilds with more than 50 members as "large".
+        // Large guilds will only provide online members in their setup and thus reduce
+        // bandwidth if chunking is disabled.
+        builder.setLargeThreshold(50);
     }
 }
