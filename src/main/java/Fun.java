17069c72ad919e282ff87b42d8baa0816fd5b276
NatsuCamellia <natsucamellia@gmail.com>
2021-12-10 20:08:27 +0800

    add 學測資訊 feature

--- src/main/java/Fun.java
+++ src/main/java/Fun.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Random;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -13,7 +15,6 @@ public class Fun extends ListenerAdapter {
         User author = event.getAuthor();
 
         if (message.contains("機率")) {
-            System.out.println("Checkpoint");
             Random random = new Random();
             int chance = random.nextInt(101); // 0 ~ 100
             EmbedBuilder builder = new EmbedBuilder();
@@ -23,6 +24,23 @@ public class Fun extends ListenerAdapter {
             builder.addField("問卜之事", message, true);
             builder.addField("結果", String.format("根據咖啡因與水分子之間的量子作用力交互作用之下，我得到了量子力學中所定義的機率，而這件事的機率為：**%d", chance) + "%**", false);
             channel.sendMessageEmbeds(builder.build()).queue();
+            return;
+        }
+
+        if (message.equals("學測")) {
+            LocalDate today = LocalDate.now();
+            LocalDate test = LocalDate.of(2022, 1, 21);
+            Period period = Period.between(today, test);
+            int days = period.getDays();
+            int months = period.getMonths();
+
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setColor(EmbedUtil.blue);
+            builder.setAuthor("學測資訊");
+            builder.addField("學測倒數", "距離學測還有 **" + String.valueOf(days + months * 31) + "** 天", true);
+            builder.setFooter(author.getAsTag(), author.getAvatarUrl());
+            channel.sendMessageEmbeds(builder.build()).queue();
+            return;
         }
     }
 }
