bc73cbc0995cf21466a958ed227cb1c48ea88566
NatsuCamellia <natsucamellia@gmail.com>
2022-01-23 19:09:01 +0800

    Delete "學測"

--- src/main/java/Text.java
+++ src/main/java/Text.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Random;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -36,25 +34,6 @@ public class Text extends ListenerAdapter {
             builder.addField("問卜者", author.getAsTag(), true);
             builder.addField("問卜之事", message, true);
             builder.addField("結果", "根據電晶體True與False之間的高速來回變換，我得出了答案為："+ tf, false);
-            channel.sendMessageEmbeds(builder.build()).queue();
-            return;
-        }
-
-        if (message.equals("學測")) {
-            LocalDate today = LocalDate.now();
-            LocalDate test = LocalDate.of(2022, 1, 21);
-            Period period = Period.between(today, test);
-            int days = period.getDays();
-            int months = period.getMonths();
-
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setColor(EmbedUtil.blue);
-            builder.setAuthor("學測資訊");
-            builder.addField("1/21(五)", "9:20 ~ 11:00 數A\n12:50 ~ 14:40 自然", true);
-            builder.addField("1/22(六)", "9:20 ~ 11:00 英文\n12:50 ~ 14:20 國綜\n15:20 ~ 16:50 國寫", true);
-            builder.addField("1/23(日)", "9:20 ~ 11:00 數B\n12:50 ~ 14:40 社會", true);
-            builder.addField("學測倒數", "距離學測還有 **" + String.valueOf(days + months * 31) + "** 天", true);
-            builder.setFooter(author.getAsTag(), author.getAvatarUrl());
             channel.sendMessageEmbeds(builder.build()).queue();
             return;
         }
