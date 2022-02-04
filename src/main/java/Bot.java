8cacc035be5f0af15a5cb76ec7f6577fc162a20f
NatsuCamellia <natsucamellia@gmail.com>
2022-02-04 21:45:19 +0800

    Update activity

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -18,7 +18,7 @@ public class Bot {
 
         JDABuilder builder = JDABuilder.createDefault(TOKEN);
 
-        builder.setActivity(Activity.playing("測試中..."));
+        builder.setActivity(Activity.playing("最新金錢功能開發中!"));
 
         builder.addEventListeners(new Command(), new Text(), new Xp(), new Economy());
 
