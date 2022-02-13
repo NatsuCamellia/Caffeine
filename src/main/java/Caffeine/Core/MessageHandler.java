ce93c66a045b7637c1c25092ee57aced5ae5d03a
NatsuCamellia <natsucamellia@gmail.com>
2022-02-13 20:41:17 +0800

    Add isCommand method

--- src/main/java/Caffeine/Core/MessageHandler.java
+++ src/main/java/Caffeine/Core/MessageHandler.java
@@ -31,40 +31,43 @@ public class MessageHandler {
         User author = event.getAuthor();
         Message message = event.getMessage();
 
-        String command = message.getContentRaw().split(" ")[0].toLowerCase();
-
         xp.add_xp(author.getId(), guild.getId());
+        
+        if (isCommand(message)) {
+            
+            String command = message.getContentRaw().split(" ")[0].toLowerCase();
+            
+            if (command.equals(Bot.prefix + "test")) {
+                System.out.println("test");
+                return;
+            }
+            
+            if (command.equals(Bot.prefix + "user")) {
+                commandExecutor.user(guild, channel, author, message);
+                return;
+            }
+            
+            if (command.equals(Bot.prefix + "help")) {
+                commandExecutor.help(channel, author);
+                return;
+            }
 
-        if (command.equals(Bot.prefix + "test")) {
-            System.out.println("test");
-            return;
+            if (command.equals(Bot.prefix + "button")) {
+                commandExecutor.button(channel);
+                return;
+            }
+            
+            if (command.equals(Bot.prefix + "coinflip")) {
+                gambling.coinflip(channel, author, message);
+                return;
+            }
+            
+            if (command.equals(Bot.prefix + "daily")) {
+                economy.daily(channel, author);
+                return;
+            }
         }
-
-        if (command.equals(Bot.prefix + "user")) {
-            commandExecutor.user(guild, channel, author, message);
-            return;
-        }
-
-        if (command.equals(Bot.prefix + "help")) {
-            commandExecutor.help(channel, author);
-            return;
-        }
-
-        if (command.equals(Bot.prefix + "button")) {
-            commandExecutor.button(channel);
-            return;
-        }
-
-        if (command.equals(Bot.prefix + "coinflip")) {
-            gambling.coinflip(channel, author, message);
-            return;
-        }
-
-        if (command.equals(Bot.prefix + "daily")) {
-            economy.daily(channel, author);
-            return;
-        }
-
+        
         if (message.getContentRaw().contains("機率")) {
             text.chance(channel, author, message);
             return;
@@ -74,6 +77,10 @@ public class MessageHandler {
             text.yes_no(channel, author, message);
             return;
         }
+    }
+
+    public boolean isCommand(Message message) {
+        return message.getContentRaw().charAt(0) == '%';
     }
 
     public void printLog(MessageReceivedEvent event) {
