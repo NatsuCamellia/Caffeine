0854f6710f2bf4b4f5e5e7bc8f3374d668834469
NatsuCamellia <natsucamellia@gmail.com>
2023-02-11 16:28:54 +0800

    Remove music commands

--- src/main/java/Caffeine/core/SlashCommandHandler.java
+++ src/main/java/Caffeine/core/SlashCommandHandler.java
@@ -2,7 +2,6 @@ package Caffeine.core;
 
 import Caffeine.inquiry.InquiryCommand;
 import Caffeine.listener.Command;
-import Caffeine.music.MusicCommand;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -11,7 +10,6 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 public class SlashCommandHandler {
 
     InquiryCommand inquiry = new InquiryCommand();
-    MusicCommand music = new MusicCommand();
     Command command = new Command();
 
     public void handle(SlashCommandInteractionEvent event) {
@@ -19,11 +17,6 @@ public class SlashCommandHandler {
         switch (event.getName()) {
             case "user" -> inquiry.user(event);
             case "guild" -> inquiry.guild(event);
-            case "play" -> music.play(event);
-            case "skip" -> music.skip(event);
-            case "queue" -> music.queue(event);
-            case "leave" -> music.leave(event);
-            case "clear" -> music.clear(event);
             case "delete" -> command.clear(event);
         }
     }
@@ -34,14 +27,8 @@ public class SlashCommandHandler {
                     Commands.slash("user", "查詢使用者資料")
                             .addOption(OptionType.MENTIONABLE, "member", "成員", true),
                     Commands.slash("guild", "查詢伺服器資料"),
-                    Commands.slash("play", "播放音樂")
-                            .addOption(OptionType.STRING, "search", "搜尋或音樂網址", true),
-                    Commands.slash("skip", "跳過曲目"),
-                    Commands.slash("queue", "查詢播放清單"),
-                    Commands.slash("leave", "使機器人離開語音頻道"),
                     Commands.slash("delete", "清除多條訊息，伺服器擁有者專用")
-                            .addOption(OptionType.INTEGER, "rows", "訊息清除的數量", true),
-                    Commands.slash("clear", "清空播放清單")
+                            .addOption(OptionType.INTEGER, "rows", "訊息清除的數量", true)
             ).complete();
         } catch (Exception e) {
             System.out.println("【Warning】【" + guild.getName() + "】沒有授予 applications.commands");
--- src/main/java/Caffeine/util/EmbedUtil.java
+++ src/main/java/Caffeine/util/EmbedUtil.java
@@ -1,25 +1,8 @@
 package Caffeine.util;
-import Caffeine.core.Bot;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
 public class EmbedUtil {
 
     public final static int BLUE = 0x5398ed;
     public final static int RED = 0xFF0000;
     public final static int GREEN = 0x6EB504;
-
-    public static void sendErrorUsageEmbed (TextChannel channel, User author, String command, String description) {
-        EmbedBuilder builder = new EmbedBuilder();
-        builder.setColor(EmbedUtil.RED);
-
-        builder.setTitle("指令錯誤：" + Bot.prefix + command);
-        builder.setDescription(description);
-
-        String avatar = author.getAvatarUrl() == null ? author.getDefaultAvatarUrl() : author.getAvatarUrl();
-        builder.setFooter(author.getAsTag(), avatar);
-
-        channel.sendMessageEmbeds(builder.build()).queue();
-    }
 }
