aa658af6dccaba9f5359396b0d95fc30d658534e
NatsuCamellia <natsucamellia@gmail.com>
2023-02-11 18:50:09 +0800

    Delete unused code

--- src/main/java/Caffeine/core/Bot.java
+++ src/main/java/Caffeine/core/Bot.java
@@ -1,15 +1,13 @@
 package Caffeine.core;
-import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Bot {
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) {
 
         String TOKEN;
 
@@ -21,9 +19,8 @@ public class Bot {
 
         JDABuilder.createDefault(TOKEN)
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES)
-                .enableCache(CacheFlag.VOICE_STATE, CacheFlag.ONLINE_STATUS)
+                .enableCache(CacheFlag.ONLINE_STATUS)
                 .setMemberCachePolicy(MemberCachePolicy.ONLINE)
-                .setActivity(Activity.playing("/play"))
                 .addEventListeners(new Listener()).build();
     }
 }
