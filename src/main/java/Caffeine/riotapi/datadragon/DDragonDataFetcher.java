e0f2c965bb7d16640205dbff67aca46e49e14461
NatsuCamellia <natsucamellia@gmail.com>
2023-07-12 13:01:36 +0800

    Use UTF-8 encoding

--- src/main/java/Caffeine/riotapi/datadragon/DDragonDataFetcher.java
+++ src/main/java/Caffeine/riotapi/datadragon/DDragonDataFetcher.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -46,8 +47,7 @@ public class DDragonDataFetcher {
             URL url = new URL(String.format(CHAMPION, getCurrentVersion()));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
 
             Champion champion = gson.fromJson(reader, Champion.class);
             reader.close();
