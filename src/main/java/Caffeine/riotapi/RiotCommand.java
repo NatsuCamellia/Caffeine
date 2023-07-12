da648efdf205d1f278e301b3fb3942ad1aceeb74
NatsuCamellia <natsucamellia@gmail.com>
2023-07-12 12:29:21 +0800

    Add mastery query

--- src/main/java/Caffeine/riotapi/RiotCommand.java
+++ src/main/java/Caffeine/riotapi/RiotCommand.java
@@ -5,8 +5,13 @@ import Caffeine.riotapi.dto.LeagueEntryDTO;
 import Caffeine.riotapi.dto.SummonerDTO;
 import Caffeine.util.EmbedUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -17,6 +22,7 @@ public class RiotCommand {
         fetcher.setAPI_KEY(API_KEY);
     }
 
+    // Get summoner info, including name, rank and top mastery
     public void getSummonerInfo(SlashCommandInteractionEvent event) {
         String summonerName = event.getOption("summoner_name").getAsString();
         SummonerDTO summoner = fetcher.getSummonerByName(summonerName);
@@ -41,18 +47,81 @@ public class RiotCommand {
             builder.addField(league.getQueueType(), league.getTier() + " " + league.getRank(), true);
         }
 
+        builder.addField("專精英雄（按專精分數）", getMasteryString(mastery, 1), false);
+
+        event.replyEmbeds(builder.build()).queue();
+    }
+
+    // Get the first page of all the mastery
+    public void getMastery(SlashCommandInteractionEvent event) {
+        String summonerName = event.getOption("summoner_name").getAsString();
+        SummonerDTO summoner = fetcher.getSummonerByName(summonerName);
+        if (summoner == null) {
+            event.replyEmbeds(summonerDNE(summonerName).build()).queue();
+            return;
+        }
+
+        List<ChampionMasteryDTO> mastery = fetcher.getMasteryBySummonerId(summoner.getId());
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle(summonerName);
+        builder.setColor(EmbedUtil.BLUE);
+
+        int page = 1;
+        builder.addField("專精英雄（按專精分數）", getMasteryString(mastery, page), false);
+        builder.setFooter(String.format("Page %d/%d", page, getMaxMasteryPage(mastery)));
+        List<Button> buttons = new ArrayList<>();
+        buttons.add(Button.primary("riot.mastery:first_page", Emoji.fromUnicode("⏪")));
+        buttons.add(Button.primary("riot.mastery:prev_page", Emoji.fromUnicode("◀")));
+        buttons.add(Button.primary("riot.mastery:next_page", Emoji.fromUnicode("▶")));
+        buttons.add(Button.primary("riot.mastery:last_page", Emoji.fromUnicode("⏩")));
+        event.replyEmbeds(builder.build()).addActionRow(buttons).queue();
+    }
+
+    // Convert list of mastery to a formatted string
+    private String getMasteryString(List<ChampionMasteryDTO> mastery, int page) {
+        page = Math.max(1, page);
+        page = Math.min(page, getMaxMasteryPage(mastery));
+        mastery = mastery.subList((page - 1) * 10, Math.min(page * 10, mastery.size()));
+
         StringBuilder masteryString = new StringBuilder();
         for (ChampionMasteryDTO champion : mastery) {
             String championName = fetcher.getChampionNameById(champion.getChampionId());
             int level = champion.getChampionLevel();
             int point = champion.getChampionPoints();
-            boolean chest = champion.isChestGranted();
-            masteryString.append(String.format("%s：專精 %d - %d 專精分｜寶箱：%s\n",
-                    championName, level, point, chest ? ":white_check_mark:" : ":negative_squared_cross_mark:"));
+            masteryString.append(String.format("專精 %d - `%7d` 專精分｜%s\n",
+                    level, point, championName));
         }
-        builder.addField("專精英雄（按專精分數）", masteryString.toString(), false);
+        return masteryString.toString();
+    }
 
-        event.replyEmbeds(builder.build()).queue();
+    public void EditMasteryEmbed(ButtonInteractionEvent event) {
+        MessageEmbed embed = event.getMessage().getEmbeds().get(0);
+        String summonerName = embed.getTitle();
+        SummonerDTO summoner = fetcher.getSummonerByName(summonerName);
+        List<ChampionMasteryDTO> mastery = fetcher.getMasteryBySummonerId(summoner.getId());
+
+        int page = Integer.parseInt(embed.getFooter().getText().split(" ")[1].split("/")[0]);
+        int lastPage = getMaxMasteryPage(mastery);
+
+        String action = event.getComponentId().split(":")[1];
+        switch (action) {
+            case "first_page" -> page = 1;
+            case "prev_page" -> page = Math.max(1, page - 1);
+            case "next_page" -> page = Math.min(page + 1, lastPage);
+            case "last_page" -> page = lastPage;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle(embed.getTitle());
+        builder.setColor(EmbedUtil.BLUE);
+        builder.setFooter(String.format("Page %d/%d", page, lastPage));
+        builder.addField("專精英雄（按專精分數）", getMasteryString(mastery, page), false);
+        event.editMessageEmbeds(builder.build()).queue();
+    }
+
+    private int getMaxMasteryPage(List<ChampionMasteryDTO> mastery) {
+        return (int)Math.ceil((double)mastery.size() / 10);
     }
 
     public EmbedBuilder summonerDNE(String summonerName) {
