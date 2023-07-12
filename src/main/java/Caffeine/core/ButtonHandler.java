b2a3ab1826f2ec2e44bc72e4f7611b026d9b3d97
NatsuCamellia <natsucamellia@gmail.com>
2023-07-12 12:30:20 +0800

    Add pagination for mastery query

--- src/main/java/Caffeine/core/ButtonHandler.java
+++ src/main/java/Caffeine/core/ButtonHandler.java
@@ -1,20 +1,27 @@
 package Caffeine.core;
 
+import Caffeine.riotapi.RiotCommand;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 public class ButtonHandler {
+
+    RiotCommand riotCommand = new RiotCommand(System.getenv("RIOT_API_KEY"));
 
     public void handle(ButtonInteractionEvent event) {
         
         String id = event.getComponentId();
 
-        if (id.equals("primary")) {
+        if (id.equals("Primary_id")) {
             event.editMessage("Primary").queue();
             return;
         }
         
-        if (id.equals("success")) {
+        if (id.equals("Success_id")) {
             event.editMessage("Success").queue();
+        }
+
+        if (id.startsWith("riot.mastery")) {
+            riotCommand.EditMasteryEmbed(event);
         }
     }
 }
